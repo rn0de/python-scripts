e30d9c6131d0297b6ef8caf7aac7d0853c209f40
Jobyer Ahmed <68237135+redtmop@users.noreply.github.com>
2023-02-01 21:14:12 +0600

    Create toHex.py

---
+++ toHex.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+import binascii
+import sys
+
+##Usage:
+### python3 toHex.py "jmp esp"
+
+string_to_convert = sys.argv[1]
+def reverse_string(string_to_convert):
+    reversed = string_to_convert[::-1]
+    getReversed = [reversed[i:i+4] for i in range(0, len(reversed), 4)]
+
+    return getReversed
+
+    getReversed = reverse_string(string_to_convert)
+    print("Result:", getReversed)
+
+
+def converter(string_to_convert):
+    tobytes = string_to_convert.encode("utf-8")
+    hexString = binascii.hexlify(tobytes).decode("utf-8")
+    blocks = [hexString[i:i+8] for i in range(0, len(hexString), 8)]
+    final_data = tuple(hex_block[6:8] + hex_block[4:6] + hex_block[2:4] + hex_block[0:2] for hex_block in blocks)
+
+    return final_data
+    
+final_data = converter(string_to_convert)
+reverse_string(string_to_convert)
+print("\nLittle-endian hexadecimal:")
+for blocks in final_data[::-1]:
+    print("0x"+blocks)
